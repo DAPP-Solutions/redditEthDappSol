eb33f0d47b0cb6e2623558ecb975c6b9b53c6d8a
Gilser <campbellj210@gmail.com>
2020-07-23 23:40:39 -0700

    SQL Schema for reddit systems
    Just the .SQL file

---
+++ api/_RedditChallenge.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE accounts(
+	id serial PRIMARY KEY,
+	pubkey text NOT NULL,
+	privkey text NOT NULL,
+	account text,
+	dateadded TIMESTAMP NOT NULL
+);
+
+create TABLE communities(
+	id serial PRIMARY KEY,
+	subname text,
+	subeth text,
+	lastrun int,
+	dateadded TIMESTAMP NOT NULL
+);
+
+create TABLE subscriptions(
+	id serial PRIMARY KEY,
+	accounteth text,
+	subname text,
+	account text,
+	balance float,
+	karma float,
+	datejoined TIMESTAMP NOT NULL
+);
+
+create TABLE approvequeue (
+	id serial PRIMARY KEY,
+	accounteth text,
+	subname text,
+	amount float,
+	lastrun int,
+	dateadded TIMESTAMP NOT NULL
+)
+
+select * from accounts
